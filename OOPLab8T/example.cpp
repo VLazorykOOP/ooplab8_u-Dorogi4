1c87df05124e4f81cf4faba7af2e2d432b1927da
Dorozhkin Oleksandr <dorozhkin.oleksandr@chnu.edu.ua>
2023-04-24 00:50:26 +0300

    2

--- OOPLab8T/example.cpp
+++ OOPLab8T/example.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <cstring>
 
 namespace Fun {
     template <typename T>
@@ -44,14 +45,68 @@ namespace Fun {
         for (int i = 0; i < 5; i++) {
             std::cout << arr1[i] << " ";
         }
+        std::cout << std::endl;
         // Виведе: 5 2 3 4 1
 
 
         return 0;
     }
 }
+namespace Stk {
+    using namespace std;
 
+    template<typename T>
+    void insertionSort(T arr[], int n) {
+        int i, j;
+        T key;
+        for (i = 1; i < n; i++) {
+            key = arr[i];
+            j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    void insertionSort(const char* arr[], int n, int (*cmp)(const char*, const char*)) {
+        int i, j;
+        const char* key;
+        for (i = 1; i < n; i++) {
+            key = arr[i];
+            j = i - 1;
+
+            while (j >= 0 && cmp(arr[j], key) > 0) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+
+    int cmp(const char* a, const char* b) {
+        return strcmp(a, b);
+    }
+
+    int smain() {
+        const char* arr[] = { "hello", "world", "this", "is", "a", "test" };
+        int n = sizeof(arr) / sizeof(arr[0]);
+
+        insertionSort(arr, n, cmp);
+
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+
+        return 0;
+    }
+}
 void example()
 {
     Fun::fmain();
+    Stk::smain();
 }
